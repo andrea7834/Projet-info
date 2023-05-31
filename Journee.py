1e9ffe9717e2affb0d1c3e55821bbf6ea13ef3d6
Andrea <andrea.arghir@ensta-bretagne.org>
2023-05-31 09:45:13 +0200

    changes

--- Journee.py
+++ Journee.py
@@ -2,30 +2,7 @@
 
 import numpy as np
 import pandas as pd
-
-
-
-
-class Club(Joueur):
-    def __init__(self, nom_club, niveau, noms_joueurs):
-        """ On définit la classe Club qui regroupe le nom de chaque club, ses joueurs,
-        son nombre de points et le nombre de buts marqués lors de la saison (initialisés à 0)
-        """
-        self.noms_joueurs = noms_joueurs
-        self.nom_club = nom_club
-        self.Joueurs = []
-        for nom in noms_joueurs:
-            super().__init__(nom)
-            self.Joueurs.append(Joueur(nom))
-        # On attribue un niveau à chaque club (en fonction des résultats de cette année)
-        self.niveau = niveau
-        self.points = 0
-        self.buts_marques = 0
-
-    def __str__(self):
-        """On définit __str__ la méthode retournant le nom de clubs, leur nombre de poinst et le nombre de buts marqués sous forme de dataframe
-        """
-        return f"Noms des clubs : {self.nom_club}, Nombre de points : {self.points}, Buts marqués : {self.buts_marques}"
+from Club import Club
 
 class Journee(list, Club):
 
